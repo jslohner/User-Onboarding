de0a7cf1ae3f54732c36c2ca5c024daafd17cb0f
Justin Lohner <lohner4@gmail.com>
2020-04-22 15:46:41 -0600

    Added formSchema, added post function, added function to check validity before submitting

--- user-onboarding/src/components/App.js
+++ user-onboarding/src/components/App.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Form from './Form.js';
+
+import axios from 'axios';
+import * as yup from 'yup';
+
+const url = 'https://reqres.in/api/users';
 
 const initUserValues = [
 	{
@@ -19,10 +24,40 @@ const initFormValues = {
 	terms: false
 }
 
+const formSchema = yup.object().shape({
+	username: yup
+		.string()
+		.min(3, 'Username must have at least 3 characters')
+		.required('Username is required'),
+	email: yup
+		.string()
+		.email('Please enter a valid email address')
+		.required('Email address is required'),
+	password: yup
+		.string()
+		.min(6, 'Password needs a minimum of 6 characters')
+		.required('Password is required'),
+	terms: yup
+		.boolean()
+		.oneOf([true], 'In order to proceed, you must agree to the terms of service by checking the checkbox')
+})
+
 function App() {
 
 	const [users, setUsers] = useState(initUserValues);
 	const [formValues, setFormValues] = useState(initFormValues);
+
+	const [submitAvailability, setSubmitAvailability] = useState(true);
+
+	const postUser = user => {
+		axios.post(url, user)
+			.then(res => {
+				setUsers([...users, res.data])
+			})
+			.catch( err => {
+				console.log('error');
+			})
+	}
 
 	const onInputChange = evt => {
 		setFormValues({
@@ -38,8 +73,25 @@ function App() {
 		})
 	}
 
-	const onSubmit = evt => {
+	useEffect(() => {
+		formSchema.isValid(formValues)
+			.then(valid => {
+				setSubmitAvailability(!valid)
+			})
+	}, [formValues])
 
+	const onSubmit = evt => {
+		evt.preventDefault();
+
+		const newUser = {
+			username: formValues.username,
+			email: formValues.email,
+			password: formValues.password,
+			terms: formValues.terms
+		}
+
+		postUser(newUser);
+		setFormValues(initFormValues);
 	}
 
 	return (
@@ -47,13 +99,9 @@ function App() {
 			<header className="App-header">
 				<h1>User Onboarding</h1>
 			</header>
-			<Form values={formValues} onInputChange={onInputChange} onCheckboxChange={onCheckboxChange}/>
+			<Form values={formValues} onInputChange={onInputChange} onCheckboxChange={onCheckboxChange} onSubmit={onSubmit} submitAvailability={submitAvailability}/>
 		</div>
 	);
 }
 
 export default App;
-
-// Submit
-// input change
-// checkbox change
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -5,24 +5,22 @@ function Form(props) {
 	const {
 		values,
 		onInputChange,
-		onCheckboxChange
+		onCheckboxChange,
+		onSubmit,
+		submitAvailability
 	} = props;
 	return (
 		<form className='form'>
 			<h2>New User</h2>
-			<label>Name: <input value={values.username} onChange={onInputChange} name='username' type='text'/></label>
+			<label>Username: <input value={values.username} onChange={onInputChange} name='username' type='text'/></label>
 			<label>Email: <input value={values.email} onChange={onInputChange} name='email' type='text'/></label>
 			<label>Password: <input value={values.password} onChange={onInputChange} name='password' type='text'/></label>
 			<label><input checked={values.terms} onChange={onCheckboxChange} name='terms' type='checkbox'/>Terms of Service</label>
-			<button>Submit</button>
+			<button onClick={onSubmit} disabled={submitAvailability}>Submit</button>
 		</form>
 	);
 }
 
 export default Form;
-//
-// - [ ] Name
-// - [ ] Email
-// - [ ] Password
-// - [ ] Terms of Service (checkbox)
+
 // - [ ] A Submit button to send our form data to the server.
